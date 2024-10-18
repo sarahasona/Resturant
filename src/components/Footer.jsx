be8f2fc607cb91aba4c9c25a7af03f1db6c41de0
SaraElbably <saraelbably121@gmail.com>
2024-10-19 01:43:23 +0300

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,22 +4,22 @@ import "./footer.css";
 function Footer() {
   return (
     <>
-      <div className="footer mt-10">
+      <div className="footer mt-4"> {/* Reduced margin-top */}
         <div className="big-con">
           <div className="firstrow">
             <div className="logo">
               <img
                 src={`${import.meta.env.BASE_URL}da logo.png`}
                 alt="Restaurant Logo"
-                className="h-56 w-56"
+                className="h-24 w-24" // Further reduced size
               />
               <p className="description">
-                The best restaurant in the world with food beyond word and world.
+                Best restaurant with food beyond words.
               </p>
             </div>
 
-            <div className="listoflinks">
-              <h3>Important Links</h3>
+            <div className="listoflinks pt-3">
+              <h3>Links</h3> {/* Shortened heading */}
               <ul>
                 <li>
                   <Link to="/">Home</Link>
@@ -40,21 +40,18 @@ function Footer() {
             </div>
 
             <div className="contact">
-              <h3>Contact Us</h3>
-              <ul>
-                <li>Email: s2yam@gmail.com</li>
-                <li>Social Media: [Links]</li>
-              </ul>
+              <h3 className="mb-3 mt-12">Contact Us</h3> 
+              <div className="links flex gap-3 mb-2"> 
+                <a href="#"> <i className="fa-brands fa-facebook icon"></i></a>
+                <a href="#"><i className="fa-brands fa-twitter icon"></i></a>
+                <a href="#"><i className="fa-brands fa-square-instagram icon"></i></a>
+              </div>
+              <a href="mailto:s2yam@gmail.com">s2yam@gmail.com</a>
             </div>
           </div>
 
           <div className="second-row">
-            <p>&copy; 2024, The Best React Developers in the World</p>
-            <div className="links">
-              <i className="fa-brands fa-facebook"></i>
-              <i className="fa-brands fa-twitter"></i>
-              <i className="fa-brands fa-discord icon"></i>
-            </div>
+            <p>&copy; 2024, Best React Developers</p> {/* Shortened text */}
           </div>
         </div>
       </div>
