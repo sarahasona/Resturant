1a496208f6773709f9236881e8c4d9260baa64d0
MEA-23 <77838294+MEA-23@users.noreply.github.com>
2024-10-23 00:18:37 +0300

    add paypal

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,11 +1,21 @@
+import Payment from "../components/Payment";
 
 function Checkout() {
   return (
-    <div>
-        HOWDY ! :D
-      
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: 600,
+      }}
+    >
+      <div style={{ width: "50%" }}>
+        <Payment amount={50} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Checkout
+export default Checkout;
